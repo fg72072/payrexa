0a8c1f66682fb4eee5ea09167516472593ae537f
Fazal <fazal@pluton.ltd>
2022-01-18 18:50:49 +0500

    fazal

--- src/screens/userpanel/trade/poststeps/Step3.js
+++ src/screens/userpanel/trade/poststeps/Step3.js
@@ -7,30 +7,45 @@ function PostStep3(){
         <Row className="align-items-end">
             <Col lg={12}>
             <Form.Group className="mb-3 position-relative" controlId="Remarks">
-            <Form.Label>Total Trading Ammount</Form.Label>
-            <Form.Control as="textarea" rows={3} type="ammount" placeholder="Lorem Ipsum" />
+            <Form.Label>Remarks (Optional)</Form.Label>
+            <Form.Control as="textarea" rows={3} type="type" placeholder="Lorem Ipsum" />
             </Form.Group>
-            <Form.Group className="mb-3 position-relative" controlId="limit">
-            <Form.Label>Order Limit</Form.Label>
-            <Form.Control type="ammount" placeholder="Limit" />
+            <Form.Group className="mb-3 position-relative" controlId="autoreply">
+            <Form.Label>Autoreply (Optional)</Form.Label>
+            <Form.Control as="textarea" rows={3} type="autoreply" placeholder="Lorem Ipsum" />
             </Form.Group>
             </Col>
         </Row>
         <hr/>
         <Row>
             <Col lg={6}>
-                <h4>Payment Method</h4>
-                <div className="mt-3 payment-btn-group">
-                <button href="#" class="small-icon-btn btn-white"><span>Bank Transefer</span> <i class="fa-solid fa-xmark"></i></button>
-                <button href="#" class="small-icon-btn small-primary-btn">+ <span>Add</span></button>
+                <h4>Counterparty Conditions</h4>
+                <div className="mt-3 ">
+                <li className="flex-div">
+                    <input type="checkbox" />
+                    <span>Complete Kyc</span>
+                </li>
+                <li className="flex-div">
+                    <input type="checkbox" />
+                    <span>Register 0 day(s) ago</span>
+                </li>
+                <li className="flex-div">
+                    <input type="checkbox" />
+                    <span>Holding more than 0.01 BTC</span>
+                </li>
                 </div>
             </Col>
             <Col lg={6}>
-            <h4>Payment Time Limit</h4>
-            <div className="mt-3 payment-btn-group">
-                <select className="small-icon-btn">
-                    <option>15 min</option>
-                </select>
+            <h4>Status</h4>
+            <div className="mt-3 ">
+            <li className="flex-div">
+                    <input type="radio" />
+                    <span>Online right now</span>
+                </li>
+                <li className="flex-div">
+                    <input type="radio" />
+                    <span>Offline manually later</span>
+                </li>
             </div>
             </Col>
         </Row>
